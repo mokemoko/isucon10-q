f2c45128d4a309614c624b6e126ce6d02fce8e55
mokemoko <mokemoko331@gmail.com>
2021-08-18 01:33:53 +0900

    update ddl

--- mysql/db/0_Schema.sql
+++ mysql/db/0_Schema.sql
@@ -17,8 +17,13 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    rev_pop     INTEGER AS (popularity * -1) STORED NOT NULL,
+    geo geometry as (ST_GeomFromText(concat("POINT(",latitude," ",longitude,")"), 4326)) STORED NOT NULL
 );
+create index idx_rent on isuumo.estate (rent);
+create index idx_rev_pop on isuumo.estate (rev_pop);
+create spatial index idx_sp on isuumo.estate (geo);
 
 CREATE TABLE isuumo.chair
 (
@@ -34,5 +39,8 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
+    rev_pop     INTEGER AS (popularity * -1) STORED NOT NULL,
     stock       INTEGER         NOT NULL
 );
+create index idx_price on isuumo.chair (price);
+create index idx_rev_pop on isuumo.chair (rev_pop);
